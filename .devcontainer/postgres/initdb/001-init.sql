3f9dbf94105dae1b5b32b2f33a4d04df78403630
dschneider-wxs <dschneider@weatherstream.com>
2026-05-26 22:19:50 +0000

    update with edr locations

--- .devcontainer/postgres/initdb/001-init.sql
+++ .devcontainer/postgres/initdb/001-init.sql
@@ -12,8 +12,6 @@ CREATE TABLE public.sensors (
 );
 
 INSERT INTO public.sensors (sensor_id, sensor_name, available_variables, valid_start_datetime, valid_end_datetime, geom) VALUES
-    ('371', 'Station 35', ARRAY['temperature', 'humidity'], '2001-10-30T14:24:55Z', '2001-10-30T14:24:55Z', ST_SetSRID(ST_MakePoint(-75, 45), 4326)),
-    ('377', 'Station 35', ARRAY['temperature', 'humidity', 'pressure'], '2002-10-30T18:31:38Z', '2002-10-30T18:31:38Z', ST_SetSRID(ST_MakePoint(-75, 45), 4326)),
-    ('238', 'Station 2147', ARRAY['temperature'], '2007-10-30T08:57:29Z', NULL, ST_SetSRID(ST_MakePoint(-79, 43), 4326)),
-    ('297', 'Station 2147', ARRAY['temperature', 'wind_speed'], '2003-10-30T07:37:29Z', '2003-10-30T07:37:29Z', ST_SetSRID(ST_MakePoint(-79, 43), 4326)),
-    ('964', 'Station 604', ARRAY['temperature', 'precipitation'], '2000-10-30T18:24:39Z', NULL, ST_SetSRID(ST_MakePoint(-122, 49), 4326));
+    ('371', 'Station 35', ARRAY['temperature', 'humidity'], '2001-10-30T14:24:55Z', '2001-10-30T14:24:55Z', ST_SetSRID(ST_MakePoint(-80.1918, 25.7617), 4326)),
+    ('377', 'Station 35', ARRAY['temperature', 'humidity', 'pressure'], '2002-10-30T18:31:38Z', '2002-10-30T18:31:38Z', ST_SetSRID(ST_MakePoint(-81.6557, 30.3322), 4326)),
+    ('964', 'Station 604', ARRAY['temperature', 'precipitation'], '2000-10-30T18:24:39Z', NULL, ST_SetSRID(ST_MakePoint(-84.3880, 33.7490), 4326));
--- wxs/pygeoapi/extensions/sensors.py
+++ wxs/pygeoapi/extensions/sensors.py
@@ -1,12 +1,16 @@
 from http import HTTPStatus
 
+from shapely.geometry import Point, shape
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session
 
+from pygeoapi.plugin import load_plugin
 from pygeoapi.provider.base import (
     ProviderGenericError,
     ProviderInvalidDataError,
+    ProviderQueryError,
 )
+from pygeoapi.provider.xarray_edr import XarrayEDRProvider
 from pygeoapi.provider.sql import PostgreSQLProvider
 
 
@@ -54,3 +58,61 @@ class SensorsPostgreSQLProvider(PostgreSQLProvider):
                 ) from err
 
         return result_id
+
+
+class SensorsLocationsEDRProvider(XarrayEDRProvider):
+    """EDR provider that maps feature locations to xarray coverage queries."""
+
+    def __init__(self, provider_def):
+        super().__init__(provider_def)
+
+        feature_provider_def = provider_def.get('feature_provider')
+        if feature_provider_def is None:
+            raise ProviderQueryError('feature_provider config is required')
+
+        self._feature_provider = load_plugin('provider', feature_provider_def)
+
+    def locations(self, select_properties=None, bbox=None, datetime_=None,
+                  location_id=None, limit=10, z=None, format_=None,
+                  instance=None, **kwargs):
+        """
+        List feature locations or query coverage at a specific location.
+
+        :returns: FeatureCollection (no location_id) or CoverageJSON
+        """
+
+        if location_id is None:
+            return self._feature_provider.query(
+                bbox=bbox or [],
+                datetime_=datetime_,
+                limit=limit,
+                resulttype='results'
+            )
+
+        feature = self._feature_provider.get(location_id)
+        if feature is None or feature.get('geometry') is None:
+            raise ProviderQueryError(f'location not found: {location_id}')
+
+        geom = shape(feature['geometry'])
+        if geom.geom_type == 'Point':
+            point = Point(geom.x, geom.y)
+        else:
+            point = geom.centroid
+
+        selected_properties = select_properties or list(self.fields.keys())
+        numeric_properties = [
+            name for name in selected_properties
+            if self.fields.get(name, {}).get('type') in ('float', 'integer')
+        ]
+
+        if not numeric_properties:
+            raise ProviderQueryError('No numeric parameters found for coverage')
+
+        return self.position(
+            wkt=point,
+            datetime_=datetime_,
+            select_properties=numeric_properties,
+            z=z,
+            format_=format_,
+            instance=instance
+        )
